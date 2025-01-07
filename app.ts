fbb8242377ba150ddb042e02be839dbf16ac7b09
nitzan <nitzan.h.2000@gmail.com>
2025-01-07 18:42:31 +0200

    chore: init project swagger

--- app.ts
+++ app.ts
@@ -1,8 +1,14 @@
+import { swaggerOptions } from "./swagger/swagger_setup";
+
 const dotenv = require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
 const app = express();
 const port = process.env.PORT;
+
+const swaggerJsdoc = require("swagger-jsdoc");
+const swaggerUi = require("swagger-ui-express");
+const specs = swaggerJsdoc(swaggerOptions);
 
 mongoose.connect(process.env.DB_CONNECT);
 const db = mongoose.connection;
@@ -12,6 +18,12 @@ db.once("open", () => console.log("Connected to database"));
 const bodyParser = require("body-parser");
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(
+  "/docs",
+  swaggerUi.serve,
+  swaggerUi.setup(specs, { explorer: true })
+);
 
 const postsRouter = require("./routes/posts_route");
 const commentsRouter = require("./routes/commants_route");
